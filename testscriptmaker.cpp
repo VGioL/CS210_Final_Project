55bdb1eb26d0296bcc3128eb9355ce1afbcaaac0
VGioL <vglee@mail.bradley.edu>
2025-05-12 23:07:35 -0500

    created files. comments with plan for milestone 4
    starting on milestone 4.
    created files testscript.csv, testscript.txt as well as testscriptmaker.cpp.
    testscript.csv and testscriptmaker.cpp will be used to produce the test script, which will be stored and accessed in testscript.txt.
    in testscriptmaker.cpp i have commented out my plan to create a test script file.

---
+++ testscriptmaker.cpp
@@ -0,0 +1,9 @@
+
+
+
+// pick and choose some cities from world_cities.cvs into testscript.csv
+// include the first three cities per country code
+// if less than three cities in one country code, include all
+// include the first city in that country twice
+// then, randomize the generated csv (order of each line)
+// then,  load csv into textscript.csv into format main.cpp will read as proper input
